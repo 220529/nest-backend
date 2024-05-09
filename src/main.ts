945fddd325f63b8ec0ef94ea882e525616321b04
liukaixin <15713857919@163.com>
2024-05-09 10:54:16 +0800

    port

--- src/main.ts
+++ src/main.ts
@@ -19,7 +19,7 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('api', app, document);
 
-  await app.listen(4000);
+  await app.listen(3302);
   console.log(`Application is running on: ${await app.getUrl()}`);
 }
 bootstrap();
