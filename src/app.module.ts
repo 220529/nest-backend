5f1655e1e9fa646d9aa7614f59136eda74a4062c
liukaixin <15713857919@163.com>
2024-05-09 10:49:18 +0800

    port

--- src/app.module.ts
+++ src/app.module.ts
@@ -17,15 +17,13 @@ import { TemplateModule } from '@/template/template.module';
       imports: [ConfigModule],
       inject: [ConfigService],
       useFactory: (configService: ConfigService) => {
-        const userName = configService.get('MONGO_INITDB_ROOT_USERNAME');
-        const passWord = configService.get('MONGO_INITDB_ROOT_PASSWORD');
-        const mongoUrl = `mongodb://${userName}:${passWord}@${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}`;
-        console.log('mongoUrl: ', mongoUrl);
+        const mongoUrl = `mongodb://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}`;
+        console.log('uri: ', mongoUrl);
         return {
-          user: userName,
-          pass: passWord,
+          user: configService.get('MONGO_INITDB_ROOT_USERNAME'),
+          pass: configService.get('MONGO_INITDB_ROOT_PASSWORD'),
           dbName: configService.get('MONGO_INITDB_DATABASE'),
-          uri: mongoUrl,
+          uri: `mongodb://${configService.get('MONGO_HOST')}:${configService.get('MONGO_PORT')}`,
         };
       },
     }),
